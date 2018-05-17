155def1edcc96048be4e0a6b9898d359712d08a7
Lyy <liyunyao1010@163.com>
2018-05-17 15:11:31 +0800

    update

--- pca.py
+++ pca.py
@@ -5,74 +5,80 @@ import numpy
 import matplotlib.pyplot as v_image
 import matplotlib.image as r_image
 
+############### 定义测试：训练比例参数 比例相加为10 贡献率P ##
+i_test = 5
+i_train = 5
+P = 0.9
+############### 读入人脸库 ######################
 train = numpy.empty((1,10304))
-xmean = numpy.empty((200,10304))
+xmean = numpy.empty((i_train*40,10304))
 for d in range(1,41):
-    for i in range(1,6):
+    for i in range(1,i_train+1):
         image = r_image.imread('./ORL/s' + str(d) + '/' + str(i) + '.jpg')
 #v_image.imshow(image,cmap='Greys_r')
 #v_image.show()
         image = image.T.flatten()
         train = numpy.vstack((train,image))
 train = train[1:,:]
+# 平均人脸
 average = train.mean(axis=0)
-for i in range(0,200):
+for i in range(0,i_train*40):
     xmean[i] = train[i] - average
+# 计算 K-L 变换的生成矩阵
 m = xmean.dot(xmean.T)
-
-
+# 求特征值 特征向量
 d,v = numpy.linalg.eig(m)
-#print(v)
-#print(d)
 evals = numpy.argsort(d)
 d = numpy.sort(-d)
 d = -d#特征值
-#v = numpy.fliplr(v)#特征向量
-#print(v.shape)
+# 降维
 dsum = d.sum()
 dsum_extract = 0
 p = -1
-#print(d)
-while dsum_extract/dsum < 0.9:
+while dsum_extract/dsum < P: # 贡献率值
     p += 1
     dsum_extract = d[0:p].sum()
-#print(p)
-vv = v[:,evals[:-p-1:-1]]
-#vv = numpy.fliplr(vv)
+print("贡献率: "+str(p))
+vv = v[:,evals[-p:]]
+vv = numpy.fliplr(vv)
 ######### 开始训练 ############
-#A = train.T
-#print(A.shape)
-#B = v[:,0:p]
-#print(B.shape)
-#C = numpy.diag(1/numpy.sqrt(d[0:p]))#numpy.diag(numpy.power(d[0:p],-float(1)/2))
-#print(C)
-base = numpy.dot(numpy.dot(train.T, vv), numpy.diag(1/numpy.sqrt(d[0:p])))
-#print(base)
+base = numpy.dot(numpy.dot(xmean.T, vv), numpy.diag(1/numpy.sqrt(d[0:p])))
 allcoor = train.dot(base)
-#print(allcoor)
-
 ######## 开始测试 ############
 accu = 0
-mdist = numpy.empty((200))
+mdist = numpy.empty((i_train*40))
 for i in range(1,41):
-    for d in range(6,11):
+    for d in range(i_train+1,11):
         image = r_image.imread('./ORL/s' + str(i) + '/' + str(d) + '.jpg')
         image = image.T.flatten()
         tcoor = image.dot(base)
-        for k in range(0,200):
+        for k in range(0,i_train*40):
             mdist[k] = numpy.linalg.norm(tcoor - allcoor[k])
         index = numpy.argsort(mdist)
-        #index = index.flatten()
-        #print(index)
-        class1 = math.floor((index[0]-1)/5) + 1
-        class2 = math.floor((index[1]-1)/5) + 1
-        class3 = math.floor((index[2]-1)/5) + 1
-        if class1 != class2 & class2 != class3:
-            classi = class1
-        elif class1 == class2:
-            classi = class1
-        elif class2 == class3:
-            classi = class2
+        ###################三阶####################
+        #print("三阶近邻:")
+        #class1 = math.floor((index[0])/i_train) + 1
+        #class2 = math.floor((index[1])/i_train) + 1
+        #class3 = math.floor((index[2])/i_train) + 1
+        #if class1 != class2 & class2 != class3:
+        #    classi = class1
+        #elif class1 == class2:
+        #    classi = class1
+        #elif class2 == class3:
+        #    classi = class2
+        ##################四阶#####################
+        print("四阶近邻:")
+        classs = numpy.array([0,0,0,0])
+        classs[0] = math.floor((index[0])/i_train) + 1
+        classs[1] = math.floor((index[1])/i_train) + 1
+        classs[2] = math.floor((index[2])/i_train) + 1
+        classs[3] = math.floor((index[3])/i_train) + 1
+        num_class = -1
+        for k in range(0,4):
+            if num_class < len(classs[classs==classs[k]]):
+                num_class = len(classs[classs==classs[k]])
+                classi = classs[k]
+        ###############################################
         if classi == i:
             accu = accu + 1
-print(accu/200)
+print(accu/(i_test*40))
